5eb16b6863c87bf436e62c19b848537512ba9170
jtschams <jtschams@yahoo.com>
2024-04-24 16:33:32 -0500

    added functions to generateMarkdown for generating license badge and generating readme.  installed inquirer v8.2.4. added functions to index.html to promt user for information and create readme file.

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function to prevent answers left blank
+const emptyAnswer = function(answer) {
+    console.log(answer);
+    if (!answer) {
+        return 'Input is required';
+    } else{
+        return true;
+    }
+}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//  Array of questions for user input
+const questions = [
+    {
+        name: "title",
+        message: "Enter your application's title.",
+        validate: emptyAnswer
+    }, {
+        name: "description",
+        message: "Enter a description for your application.",
+        validate: emptyAnswer
+    }, {
+        name: "install",
+        message: "Enter any installation instructions, or leave blank if none required.",
+        default: 'No installation required',
+    }, {
+        name: "usage",
+        message: "Enter any instructions for usage.",
+        validate: emptyAnswer
+    }, {
+        name: "contribute",
+        message: "Enter instructions for anyone who wishes to contribute.",
+        validate: emptyAnswer
+    }, {
+        name: "test",
+        message: "Enter any instructions for testing application.",
+        validate: emptyAnswer
+    }, {
+        name: "username",
+        message:"Enter your Github username.",
+        validate: emptyAnswer
+    }, {
+        name: "email",
+        message: "Enter your email address.",
+        validate: emptyAnswer
+    }, {
+        name: "license",
+        type: "list",
+        message: "Specify which license your application is made under",
+        choices: ['MIT', 'GNU GPLv3', {name: "no license", value: ""}],
+        default: 2
+    }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => err ? console.error('Unable to write file.') : console.log(`Created README.`))
+}
+
+// Initialize app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const readme = generateMarkdown(answers)
+            writeToFile('./README.md', readme)
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,101 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// Function does not get called if there is no license
+function renderLicenseBadge(license) {
+  const link = renderLicenseLink(license);
+  let badgeLicense;
+  switch (license) {
+    case 'MIT':
+      badgeLicense = 'MIT-yellow.svg';
+      break;
+    case 'GNU':
+      badgeLicense = 'GPLv3-blue.svg';
+      break;
+    default: console.error('Error creating license badge')
+  }
+  const badge = `[![License: ${license}](https://img.shields.io/badge/License-${badgeLicense})](${link})`;
+  return badge;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+// Function does not get called if there is no license
+function renderLicenseLink(license) {
+  let link;
+  switch (license) {
+    case 'MIT':
+      link = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'GNU':
+      link = 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    default: console.error('Error creating license badge')
+  }
+  return link;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+// If there is no license, skip calling other license functions return an empty string
+function renderLicenseSection(license) {
+  if (license) {
+    const badge = renderLicenseBadge(license);
+    const section = `## License
+    
+${badge}
 
-// TODO: Create a function to generate markdown for README
+The license used is ${license}, which can be viewed in the repository.`
+    return {badge, section}
+  } else {
+    return '';
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
+
+  const license = renderLicenseSection(data.license)
+
   return `# ${data.title}
 
+${license.badge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#Installation)
+
+- [Usage](#Usage)
+
+- [Contribute](#Contributing)
+
+- [Tests](#Tests)
+
+- [Questions](#Questions)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+My Github account can be found at [${data.username}](https://github.com/${data.username}).
+
+For any other questions, please contact me at ${data.email}.
+
+${license.section}
 `;
 }
 
